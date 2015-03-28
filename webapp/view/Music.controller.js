7a18897a6791dda146b585756efaaf81fe59746f
Séverin de Beaulieu <severin.debeaulieu@gmail.com>
2015-03-28 23:44:34 +0100

    Carousel pour les vues de l'application

--- webapp/view/Music.controller.js
+++ webapp/view/Music.controller.js
@@ -8,9 +8,26 @@
 
 		onInit: function () {
 			this.oModel = new sap.ui.model.json.JSONModel({
+				//MUSIC PAGE
 				newMusicSearch: "",
 				itunesResult: {},
-				play: {}
+				play: {},
+
+
+				//TODO PAGE
+				newTodo: "",
+				todos: [
+					{
+						title: "Start this app",
+						completed: true
+					},
+					{
+						title: "Learn OpenUI5",
+						completed: false
+					}
+				],
+				someCompleted: true,
+				completedCount: 1
 			});
 
 			this.getView().setModel(this.oModel);
@@ -18,11 +35,27 @@
 
 		},
 
+
+		/*
+		 * **********************************************************************
+		 * 						MUSIC METHODS
+		 * **********************************************************************
+		 */
+
+
 		onSearchMusic: function () {
 			//var url = 'http://localhost:8080/proxy/search?term=' + encodeURI(this.oModel.getProperty("/newMusicSearch"))
 			//	+ '&media=music&entity=song';
 			var url = 'https://itunes.apple.com/search?term=' + encodeURI(this.oModel.getProperty("/newMusicSearch"))
-					+ '&media=music&entity=song';
+				+ '&media=music&entity=song&callback=?';
+
+
+			/*jQuery.getJSON(url, function(data){
+			 this.oModel.setProperty("/itunesResult", data);
+			 console.log(this.oModel);
+			 this.oModel.setProperty("/newMusicSearch", "");
+			 this.oModel.refresh();
+			 }.bind(this));*/
 
 
 			jQuery.ajax({
@@ -55,7 +88,7 @@
 				playSong = oModel.getProperty(e.mParameters.listItem.getBindingContextPath());
 			this.oModel.setProperty("/play", playSong);
 			this.oModel.refresh();
-			if (! this._oDialog) {
+			if (!this._oDialog) {
 				this._oDialog = sap.ui.xmlfragment("demo.fragments.playerDialog", this);
 				this.getView().addDependent(this._oDialog);
 			}
@@ -69,6 +102,59 @@
 		onCloseDialog: function (oEvent) {
 			jQuery("#trackPlayer")[0].pause();
 			this._oDialog.close();
+		},
+
+
+
+
+
+		/*
+		 * **********************************************************************
+		 * 						TODO LIST METHODS
+		 * **********************************************************************
+		 */
+
+		addTodo: function () {
+			var aTodos = this.oModel.getObject("/todos");
+			aTodos.unshift({
+				title: this.oModel.getProperty("/newTodo"),
+				completed: false
+			});
+			this.oModel.setProperty("/newTodo", "");
+			this.oModel.refresh();
+		},
+
+		toggleCompleted: function () {
+			var iCompletedCount = 0;
+			var aTodos = this.oModel.getObject("/todos");
+			var i = aTodos.length;
+			while (i--) {
+				var oTodo = aTodos[i];
+				if (oTodo.completed) {
+					iCompletedCount++;
+				}
+			}
+			this.setCompletedCount(iCompletedCount);
+			this.oModel.refresh();
+		},
+
+		clearCompleted: function () {
+			var aTodos = this.oModel.getObject("/todos");
+			var i = aTodos.length;
+			while (i--) {
+				var oTodo = aTodos[i];
+				if (oTodo.completed) {
+					aTodos.splice(i, 1);
+				}
+			}
+			this.setCompletedCount(0);
+			this.oModel.refresh();
+		},
+
+		setCompletedCount: function (iCount) {
+			this.oModel.setProperty("/completedCount", iCount);
+			this.oModel.setProperty("/someCompleted", iCount > 0);
+			this.oModel.refresh();
 		}
 
 	});
